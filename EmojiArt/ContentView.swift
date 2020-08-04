eecc6953386ffb9da82cca63484d7f75afc87164
yoonsoopark <ypark9@ncsu.edu>
2020-08-03 22:51:37 -0400

    Initial Commit

---
+++ EmojiArt/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  EmojiArt
+//
+//  Created by Yoonsoo Park on 8/3/20.
+//  Copyright © 2020 Yoonsoo Park. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
