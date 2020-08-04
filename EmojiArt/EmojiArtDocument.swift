e50ea587011b759cf5dee1de5a5dfbd38285fd78
yoonsoopark <ypark9@ncsu.edu>
2020-08-03 23:12:12 -0400

    Init EmojiArt

---
+++ EmojiArt/EmojiArtDocument.swift
@@ -0,0 +1,13 @@
+//
+//  EmojiArtDocument.swift
+//  EmojiArt
+//
+//  Created by Yoonsoo Park on 8/3/20.
+//  Copyright © 2020 Yoonsoo Park. All rights reserved.
+//
+
+import SwiftUI
+
+class EmojiArtDocument : ObservableObject {
+    
+}
